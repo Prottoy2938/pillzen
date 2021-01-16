62d1ff01bd91ba7ae9b16b94f2aa5dec1a5abe90
Prottay Rudra <prottoyrudra7777@gmail.com>
2021-01-16 23:11:50 +0600

    Created the basic homepage - moved to 'development' branch - installed chakra-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import Hello from "../src/components/hello/hello";
+import { Heading, Button, Box } from "@chakra-ui/react";
 
 const Home: React.FC = () => {
-  return <Hello name="World!" />;
+  return (
+    <Box display="table" m="0 auto">
+      <Heading display="table" m="0 auto" mb="50px">
+        Pillzone
+      </Heading>
+      <Heading fontSize="xl">Some App Description Here</Heading>
+      <a href="/new-prescription">
+        <Button display="table" m="0 auto" mt="100px">
+          Get Started
+        </Button>
+      </a>
+    </Box>
+  );
 };
 
 export default Home;
