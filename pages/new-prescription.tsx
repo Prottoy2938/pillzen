99ae018d71d54f5aa39104f2a6dee4ead793a569
Prottay Rudra <prottoyrudra7777@gmail.com>
2021-01-17 00:08:56 +0600

    Adding functionality to the prescription page - styled it

--- pages/new-prescription.tsx
+++ pages/new-prescription.tsx
@@ -1,8 +1,33 @@
-import React from "react";
-import { Heading, Button, Box, Grid, GridItem } from "@chakra-ui/react";
+import React, { useCallback, useState } from "react";
+import {
+  Heading,
+  Button,
+  Box,
+  Grid,
+  GridItem,
+  Text,
+  Image,
+} from "@chakra-ui/react";
 import Head from "next/head";
+import { useDropzone } from "react-dropzone";
+import { FaFileImport } from "react-icons/fa";
 
 const Home: React.FC = () => {
+  const [selectedFile, setSelectedFile] = useState<File>();
+
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+    // @ts-ignore
+    setSelectedFile(acceptedFiles[0]);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    multiple: false,
+    maxSize: 5242880, //5mb
+    accept: "image/*",
+  });
+
   return (
     <>
       <Head>
@@ -15,13 +40,69 @@ const Home: React.FC = () => {
         </Heading>
         <Grid templateColumns="repeat(6, 1fr)" gap={4}>
           <GridItem colSpan={3}>
-            <Box>
-              <Heading mb="50px">Upload Image</Heading>
-            </Box>
+            <Heading>Prescription Image</Heading>
+            <Grid mt="70px" templateColumns="repeat(6, 1fr)" gap={4}>
+              <GridItem m="0 auto" colSpan={3}>
+                <Box>
+                  <Heading size="md" mb="30px">
+                    Upload Image
+                  </Heading>
+                  <Box
+                    {...getRootProps()}
+                    height="200px"
+                    border="1px dashed"
+                    textAlign="center"
+                    cursor="pointer"
+                    pr={2}
+                    pl={2}
+                  >
+                    <input {...getInputProps()} />
+                    <Box pos="relative" top="15%">
+                      <FaFileImport
+                        style={{
+                          width: "55px",
+                          height: "55px",
+                          margin: "0 auto",
+                        }}
+                      />
+                      <Text marginTop="20px">
+                        Click or drag file to this area to upload
+                      </Text>
+                    </Box>
+                  </Box>
+                </Box>
+              </GridItem>
+              <GridItem m="0 auto" colSpan={3}>
+                <Box>
+                  <Heading size="md">Capture Image</Heading>
+                </Box>
+              </GridItem>
+            </Grid>
+            <Button
+              isDisabled={!selectedFile}
+              pr={20}
+              pl={20}
+              bg="rgba(16, 181, 60, 0.7)"
+              display="table"
+              m="100px auto"
+              _hover={{
+                background: "rgba(16, 181, 60, 0.9)",
+              }}
+            >
+              Submit
+            </Button>
           </GridItem>
-          <GridItem colSpan={3}>
-            {" "}
-            <Heading>Preview</Heading>
+          <GridItem m="0 auto" colSpan={3}>
+            <Heading mb="30px">Preview</Heading>
+            {selectedFile ? (
+              <Box>
+                <Image
+                  w="80%"
+                  src={URL.createObjectURL(selectedFile)}
+                  alt="Uploaded Image"
+                />
+              </Box>
+            ) : null}
           </GridItem>
         </Grid>
       </Box>
